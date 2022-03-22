72e27ff82e298b47b265a856279d7855d9eb2e2f
avariiinaa <log7548@gmail.com>
2022-03-22 23:47:05 +0300

    rte

---
+++ tbot/main.py
@@ -0,0 +1,19 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import logging
+
+TOKEN = "5254891313:AAEJ8i-OTsmUgonyhj3FHlLa9KE7_kx6CTw"
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+logging.basicConfig(format="%(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+
+
+def text(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message['text'])
+
+
+text_handler = MessageHandler(Filters.text, text)
+dispatcher.add_handler(text_handler)
+
+updater.start_polling()
